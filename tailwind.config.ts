55cd3942ef296e3dceed36b20b332a55aa3a5193
Jesutooni <edeworuyoyou@gmail.com>
2024-02-10 12:13:02 +0100

    tailwind custom colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,37 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        green: {
+          DEFAULT: "#CAFF33",
+          65: "#D1FF4C",
+          70: "#D8FF66",
+          80: "#E5FF99",
+          90: "#F2FFCC",
+          95: "#F8FFE5",
+          97: "#FBFFF0",
+          99: "#FEFFFA",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          90: "#E4E4E7",
+          95: "#F1F1F3",
+          97: "#F7F7F8",
+          99: "#FCFCFD",
+        },
+        grey: {
+          10: '#191919',
+          11: '#1C1C1C',
+          15: '#262626',
+          20: '#333333',
+          30: '#4C4C4D',
+          35: '#59595A',
+          40: '#656567',
+          60: '#98989A',
+          70: '#B3B3B3',
+          75: '#BFBFBF',
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
