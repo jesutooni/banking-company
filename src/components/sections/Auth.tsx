dfc72d5a1004e44270a844d08048899e5cbfded4
Jesutooni <edeworuyoyou@gmail.com>
2024-05-17 22:36:00 +0100

    auth backgrounds

--- src/components/sections/Auth.tsx
+++ src/components/sections/Auth.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Icon from "../global/Icon";
 
 const Auth = ({
@@ -10,23 +11,42 @@ const Auth = ({
   children: React.ReactNode;
 }) => {
   return (
-    <section className="max-w-5xl mx-auto w-full py-20 text-center">
-      <div className="mb-[60px]">
-        <h1 className="text-green capitalize text-4xl font-medium">
-          {heading}
-        </h1>
-        <p className="mt-4 text-grey-75">{teaser}</p>
-      </div>
-      {children}
-      <div className="flex items-center justify-center gap-4 mt-6">
-        <div className="h-[1px] w-32 bg-grey-70"></div>
-        <p className="text-sm text-grey-70">Or Continue with</p>
-        <div className="h-[1px] w-32 bg-grey-70"></div>
-      </div>
-      <div className="mt-6 flex justify-center items-center gap-5">
-        <Icon name="AuthGoogle" size={6} />
-        <Icon name="AuthFacebook" size={6} />
-        <Icon name="AuthApple" size={6} />
+    <section className="mx-4 mt-[30px] md:mt-[50px]">
+      <div className="max-w-5xl mx-auto w-full py-20 text-center  border border-grey-15 rounded-2xl px-[30px] relative">
+        <div className="absolute top-0 right-0 z-10">
+          <Image
+            src="/assets/abstract.svg"
+            alt="abstract design"
+            height={200}
+            width={200}
+          />
+        </div>
+        <Image
+          className="absolute inset-0 opacity-30 mix-blend-overlay"
+          src="/assets/auth-background.png"
+          layout="fill"
+          objectFit="cover"
+          alt="background"
+        />
+        <div className="mb-[60px]">
+          <h1 className="text-green capitalize text-[28px] md:text-4xl font-medium">
+            {heading}
+          </h1>
+          <p className="mt-2.5 md:mt-4 text-grey-75 text-sm md:text-base">
+            {teaser}
+          </p>
+        </div>
+        {children}
+        <div className="flex items-center justify-center gap-4 mt-6 max-w-[404px] mx-auto">
+          <div className="h-[1px] w-full bg-grey-70"></div>
+          <p className="text-sm text-grey-70 w-full">Or Continue with</p>
+          <div className="h-[1px] w-full bg-grey-70"></div>
+        </div>
+        <div className="mt-6 flex justify-center items-center gap-5">
+          <Icon name="AuthGoogle" size={6} />
+          <Icon name="AuthFacebook" size={6} />
+          <Icon name="AuthApple" size={6} />
+        </div>
       </div>
     </section>
   );
